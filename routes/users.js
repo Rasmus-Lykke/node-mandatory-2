03e0e05ff0f15d39d592b394ec014969d9d3f6b8
Rasmus-Lykke <rasmusls98@gmail.com>
2020-05-15 04:00:37 +0200

    Small changes to stuff like color

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const User = require('../models/User.js');
 
+// Async function
 router.get('/users', async (req, res) => {
     const allUsers = await User.query().select('username');
     return res.send({ response: allUsers });
